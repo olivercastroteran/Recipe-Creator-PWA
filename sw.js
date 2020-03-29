14f03931c0dfaad98735de069b02f08ba841ac3d
olivercastroteran <olivercastroteran@gmail.com>
2020-03-29 13:01:18 -0400

    conditional fallback page added

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //'https://cors-anywhere.herokuapp.com/https://fonts.googleapis.com/icon?family=Materializa+Icons'
 const staticCacheName = 'site-static-v1';
-const dynamicCache = 'site-dynamic-v1';
+const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
   '/',
   '/index.html',
@@ -11,7 +11,8 @@ const assets = [
   '/css/materialize.min.css',
   '/img/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+  'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  '/pages/fallback.html'
 ];
 
 // install event
@@ -32,7 +33,7 @@ self.addEventListener('activate', evt => {
     caches.keys().then(keys => {
       return Promise.all(
         keys
-          .filter(key => key !== staticCacheName)
+          .filter(key => key !== staticCacheName && key !== dynamicCacheName)
           .map(key => caches.delete(key))
       );
     })
@@ -43,16 +44,23 @@ self.addEventListener('activate', evt => {
 self.addEventListener('fetch', evt => {
   //console.log('fetch event', evt);
   evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return (
-        cacheRes ||
-        fetch(evt.request).then(fetchRes => {
-          return caches.open(dynamicCache).then(cache => {
-            cache.put(evt.request.url, fetchRes.clone());
-            return fetchRes;
-          });
-        })
-      );
-    })
+    caches
+      .match(evt.request)
+      .then(cacheRes => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then(fetchRes => {
+            return caches.open(dynamicCacheName).then(cache => {
+              cache.put(evt.request.url, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf('.html') > -1) {
+          return caches.match('/pages/fallback.html');
+        }
+      })
   );
 });
