77e71197c0a62d7bfc437f4482975c1f88fdad7a
olivercastroteran <olivercastroteran@gmail.com>
2020-03-29 13:48:55 -0400

    cache size limited

--- sw.js
+++ sw.js
@@ -15,6 +15,17 @@ const assets = [
   '/pages/fallback.html'
 ];
 
+// Cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
 // install event
 self.addEventListener('install', evt => {
   //console.log('service worker installed');
@@ -52,6 +63,7 @@ self.addEventListener('fetch', evt => {
           fetch(evt.request).then(fetchRes => {
             return caches.open(dynamicCacheName).then(cache => {
               cache.put(evt.request.url, fetchRes.clone());
+              limitCacheSize(dynamicCacheName, 15);
               return fetchRes;
             });
           })
